69ea6c529f88ee3591ca76b9756b1ab694bdfeb4
SomarGuy <diogo.ramos@me.com>
2024-07-23 23:22:52 -0400

    Blog Post

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BlogPost from './components/BlogPost';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BlogPost />
+    </div>
+  );
+};
+
+export default App;
